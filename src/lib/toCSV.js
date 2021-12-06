626318d383af7dcdfb47061c5c04249933076803
Brian <brianflaherty64@gmail.com>
2021-12-06 14:06:07 -0500

    feat: csv parser now properly orders columns if object keys are not ordered consistently

--- src/lib/toCSV.js
+++ src/lib/toCSV.js
@@ -1,20 +1,37 @@
+import get from 'lodash/get';
 import { parse } from 'json2csv';
 import { of } from 'rxjs';
-import { mergeMap, scan } from 'rxjs/operators';
+import { map, mergeMap, scan } from 'rxjs/operators';
 
 const defaultOptions = {
   parserOptions: {},
 };
 
+export const reorderObjectKeys = (keys, row) => keys.reduce((acc, key) => ({
+  ...acc,
+  [key]: get(row, `[${key}]`, null)
+}), {});
+
+export const reduceIndexAndColumnNames = () => (acc, row) => [
+  acc[0] + 1,
+  row,
+  // get the column headings from the first object
+  acc[0] === -1 ? Object.keys(row) : acc[2],
+];
+
 const toCSV = function toCSV(options = {}) {
   const config = {...defaultOptions, ...options};
   return object$ => object$.pipe(
-     scan(([index], row) => [index + 1, row], [-1, null]),
-     mergeMap(([index, row]) => (
-       index === 0
-       ? of(parse(row, config.parserOptions))
-       : of(parse(row, {...config.parserOptions, header: false}))
-     ))
+    scan(reduceIndexAndColumnNames(), [-1, null, null]),
+    map(([index, row, columnNames]) => [
+      index,
+      reorderObjectKeys(columnNames, row)
+    ]),
+    mergeMap(([index, row]) => (
+      index === 0
+      ? of(parse(row, config.parserOptions))
+      : of(parse(row, {...config.parserOptions, header: false}))
+    ))
   );
 };
 
--- src/lib/toCSV.test.js
+++ src/lib/toCSV.test.js
@@ -1,7 +1,7 @@
 import {expect} from 'chai';
 import {marbles} from 'rxjs-marbles/mocha';
 
-import toCsv from './toCsv';
+import toCsv, {reorderObjectKeys, reduceIndexAndColumnNames} from './toCsv';
 
 const rows = [
  {carModel: 'Audi', price: 10000, color: 'blue'},
@@ -23,5 +23,30 @@ describe('operators.toCsv', () => {
    const csvRow$ = row$.pipe(toCsv());
    const expected$ = m.cold('-0-12---(3|)', csvStr);
    m.expect(csvRow$).toBeObservable(expected$);
+ }));
+
+ it('should reorder object keys correctly', () => {
+   const params = {foo: 0, bar: 1};
+   const result = reorderObjectKeys(['bar', 'foo'], params);
+   expect(Object.keys(result)).to.deep.equal(['bar', 'foo']);
+   expect(result).to.deep.equal({bar: 1, foo: 0});
+ });
+
+ it('should handle case where the input objects mix up the order of their keys', marbles(m => {
+  const row$ = m.cold('-0-12---(34|)', [
+    {price: 1000000, color: 'white', carModel: 'Maserati'},
+    ...rows,
+    {color: 'blue', price: 250000, carModel: 'Ferrari'},
+  ]);
+  const csvRow$ = row$.pipe(toCsv());
+  const expected$ = m.cold('-0-12---(34|)', [
+    `"price","color","carModel"\n1000000,"white","Maserati"`,
+    '10000,"blue","Audi"',
+    '15000,"red","BMW"',
+    '20000,"yellow","Mercedes"',
+    '30000,"green","Porsche"',
+    '250000,"blue","Ferrari"',
+  ]);
+  m.expect(csvRow$).toBeObservable(expected$);
  }));
 });
