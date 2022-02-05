361078da76b5203b87d818e3f6308b99320a5e3a
Brian <brianflaherty64@gmail.com>
2022-02-05 10:58:33 -0500

    fix: resolved issue where fatal exceptions would occur if upstream observable had an error

--- src/lib/parseCSV.js
+++ src/lib/parseCSV.js
@@ -1,8 +1,20 @@
+import get from 'lodash/get';
 import {concat,Observable} from 'rxjs';
 // import csvParse from 'csv-parse/lib/sync';
 import csvParse from 'csv-parse';
-import {rxToStream} from 'rxjs-stream';
-import {bufferCount,filter,map,mergeMap,scan,tap} from 'rxjs/operators';
+// import {rxToStream} from 'rxjs-stream';
+import {throwError} from 'rxjs';
+import {
+  bufferCount,
+  catchError,
+  filter,
+  map,
+  mergeMap,
+  scan,
+  tap
+} from 'rxjs/operators';
+
+import {streamFromObservable} from '@buccaneerai/rxjs-streams';
 
 // see https://csv.js.org/parse/options/
 const defaultOptions = {
@@ -48,42 +60,76 @@ const defaultOptions = {
 //  );
 // };
 
-const parse = function parse(options = {}) {
-  const config = {...defaultOptions, ...options};
-  return csvStr$ => {
-    const row$ = new Observable(obs => {
-      let bufferedCSV$, rs;
-      try {
-        bufferedCSV$ = (
-          config.bufferInput
-          ? csvStr$.pipe(
-            config.bufferOperator(...config.bufferArgs),
-            map(strings => strings.reduce((acc, str) => `${acc}${str}`, ''))
-          )
-          : csvStr$
-        );
-        rs = rxToStream(bufferedCSV$);
-      } catch (e) {
-        return obs.error(e);
-      }
+const parseStream = (bufferedCSV$, parser, obs) => new Promise((resolve, reject) => {
+  try {
+    const rs = streamFromObservable(bufferedCSV$);
+    return rs.pipe(parser)
+      .on('data', row => obs.next(row))
+      .on('error', err => reject(err))
+      .on('end', () => resolve());
+  } catch (err) {
+    return reject(err);
+  }
+});
 
-      // https://csv.js.org/parse/recipies/stream_pipe/
-      const parser = csvParse(config.parserOptions);
-      // parser.on('readable', () => {
-      //   let record;
-      //   while (record = parser.read()) {
-      //     obs.next(record)
-      //   }
-      // });
-      // parser.on('error', err => obs.error(err))
-      // parser.on('end', () => obs.complete());
+const parse = (options = {}) => csvStr$ => {
+  try {
+    const config = {...defaultOptions, ...options};
+    const row$ = new Observable(obs => {
+      const bufferedCSV$ = (
+        config.bufferInput
+        ? csvStr$.pipe(
+          config.bufferOperator(...config.bufferArgs),
+          map(strings => strings.reduce((acc, str) => `${acc}${str}`, ''))
+        )
+        : csvStr$
+      );
+      const rs = streamFromObservable(bufferedCSV$);
+      const parser = csvParse(get(config, 'parserOptions', {}));
+      rs.on('error', err => obs.error(err));
       rs.pipe(parser)
+        .on('error', err => {
+          console.log('EMITTED ERROR');
+          console.trace(err);
+          obs.error(err);
+        })
         .on('data', row => obs.next(row))
-        .on('error', err => obs.error(err))
         .on('end', () => obs.complete());
     });
     return row$;
-  };
-}
+  } catch (err) {
+    return throwError(err);
+  }
+};
+  // const parser = csvParse(config.parserOptions);
+  // const promise = parseStream(bufferedCSV$, parser, obs);
+  // promise
+  //   .then(() => obs.complete())
+  //   .catch(err => obs.error(err));
+  // rs.pipe(parser)
+  //   .on('data', row => obs.next(row))
+  //   .on('error', err => obs.error(err))
+  //   .on('end', () => obs.complete());
+  // https://csv.js.org/parse/recipies/stream_pipe/
+  // parser.on('readable', () => {
+  //   let record;
+  //   while (record = parser.read()) {
+  //     obs.next(record)
+  //   }
+  // });
+  // parser.on('end', () => obs.complete());
+  // parser.on('error', err => obs.error(err));
+  //   try {
+  //     (async function () {
+  //       const parser = csvParse(config.parserOptions);
+  //       const promise = parseStream(bufferedCSV$, parser, obs);
+  //       promise
+  //         .then(() => obs.complete())
+  //         .catch(error => obs.error(error));
+  //       return promise;
+  //     })();
+  //   } catch (err) {
+  //     obs.error(err);
+  //   }
 
 export default parse;
--- src/lib/parseCSV.test.js
+++ src/lib/parseCSV.test.js
@@ -1,8 +1,11 @@
 import {expect} from 'chai';
 import sinon from 'sinon';
 import {marbles} from 'rxjs-marbles/mocha';
+import fs from 'fs';
+import path from 'path';
 import get from 'lodash/get';
-import {of} from 'rxjs';
+import {from,of,throwError} from 'rxjs';
+import {mergeMap} from 'rxjs/operators';
 
 import parseCSV from './parseCSV';
 
@@ -27,16 +30,21 @@ const rows = [
   {name: 'Charles Vayne', systolicBp: 200, diastolicBp: 100, motto: 'Stab first ask questions later'}
 ];
 
+const sampleCSVStrings = fs.readFileSync(
+  path.resolve(__dirname, '../../data/valid-data.csv')
+).toString().split('\n');
+
+
 describe('operators.parseCSV', () => {
   it('should produce correct row objects when given simple CSV strings', done => {
     expect(rows.length + 2);
     const onData = sinon.spy();
-    const onError = sinon.spy();
+    // const onError = sinon.spy();
     const csv$ = of(...simpleCsvStrings);
     const row$ = csv$.pipe(parseCSV({bufferInput:false}));
-    row$.subscribe(onData, onData, () => {
-      if (onError.calledOnce) console.error(onError.getCall(0).args[0]);
-      expect(onError.called).to.be.false;
+    row$.subscribe(onData, console.trace, () => {
+      // if (onError.calledOnce) console.error(onError.getCall(0).args[0]);
+      // expect(onError.called).to.be.false;
       expect(onData.callCount).to.equal(rows.length);
       rows.forEach((row, i) => {
         expect(onData.getCall(i).args[0]).to.deep.equal(row);
@@ -48,12 +56,12 @@ describe('operators.parseCSV', () => {
   it('should produce correct row objects when given simple CSV strings', done => {
     expect(rows.length + 2);
     const onData = sinon.spy();
-    const onError = sinon.spy();
+    // const onError = sinon.spy();
     const csv$ = of(...csvStringFragments);
     const row$ = csv$.pipe(parseCSV({bufferInput:false}));
-    row$.subscribe(onData, onData, () => {
-      if (onError.calledOnce) console.error(onError.getCall(0).args[0]);
-      expect(onError.called).to.be.false;
+    row$.subscribe(onData, console.trace, () => {
+      // if (onError.calledOnce) console.error(onError.getCall(0).args[0]);
+      // expect(onError.called).to.be.false;
       expect(onData.callCount).to.equal(rows.length);
       rows.forEach((row, i) => {
         expect(onData.getCall(i).args[0]).to.deep.equal(row);
@@ -65,12 +73,12 @@ describe('operators.parseCSV', () => {
   it('should produce correct row objects when CSV strings are buffered', done => {
     expect(rows.length + 2);
     const onData = sinon.spy();
-    const onError = sinon.spy();
+    // const onError = sinon.spy();
     const csv$ = of(...csvStringFragments);
     const row$ = csv$.pipe(parseCSV({bufferInput:true, bufferSize: 3}));
-    row$.subscribe(onData, onData, () => {
-      if (onError.calledOnce) console.error(onError.getCall(0).args[0]);
-      expect(onError.called).to.be.false;
+    row$.subscribe(onData, console.trace, () => {
+      // if (onError.calledOnce) console.error(onError.getCall(0).args[0]);
+      // expect(onError.called).to.be.false;
       expect(onData.callCount).to.equal(rows.length);
       rows.forEach((row, i) => {
         expect(onData.getCall(i).args[0]).to.deep.equal(row);
@@ -78,6 +86,129 @@ describe('operators.parseCSV', () => {
       done();
     });
   }).timeout(2000);
+
+  it('upstream operator should not throw', done => {
+    const input$ = of(...simpleCsvStrings);
+    const error = new Error('This is a promise rejection');
+    const rejectedPromise = () => from(
+      new Promise((resolve, reject) => reject(error))
+    );
+    const runTestWithoutParser = () => {
+      const out$ = input$.pipe(
+        mergeMap(() => rejectedPromise()),
+      );
+      out$.subscribe(
+        null,
+        err => {
+          expect(err).to.be.an('error');
+          done();
+        })
+    };
+    expect(runTestWithoutParser).to.not.throw();
+  }).timeout(2000);
+
+  it('should not throw when upstream operator throws', done => {
+    const input$ = of(...simpleCsvStrings);
+    const error = new Error('this is an uncaught exception');
+    const badThings = () => {throw error;};
+    const runTestWithParser = () => {
+      const out$ = input$.pipe(
+        mergeMap(() => badThings()),
+        parseCSV()
+      );
+      out$.subscribe(
+        null,
+        err => {
+          expect(err).to.be.an('error');
+          done();
+        })
+    };
+    expect(runTestWithParser).to.not.throw();
+  }).timeout(2000);
+
+  it('should not throw when upstream operator emits an error', done => {
+    const input$ = of(...simpleCsvStrings);
+    const error = new Error('RxJS caught error!');
+    const badThings = () => throwError(error);
+    const runTestWithParser = () => {
+      const out$ = input$.pipe(
+        mergeMap(badThings),
+        parseCSV()
+      );
+      out$.subscribe(
+        null,
+        err => {
+          expect(err).to.be.an('error');
+          done();
+        }
+      );
+    };
+    expect(runTestWithParser).to.not.throw();
+  }).timeout(2000);
+
+  it('should not throw fatal exception when upstream operator has a rejected promise', done => {
+    const input$ = of(...simpleCsvStrings);
+    const error = new Error('This is a promise rejection!');
+    const rejectedPromise = () => from(
+      new Promise((resolve, reject) => {
+        reject(error);
+      })
+    );
+    const runTestWithParser = () => {
+      const out$ = input$.pipe(
+        mergeMap(() => rejectedPromise()),
+        parseCSV()
+      );
+      out$.subscribe(null, err => {
+        expect(err).to.be.an('error');
+        done();
+      });
+    };
+    expect(runTestWithParser).to.not.throw();
+  }).timeout(2000);
+
+  it('should properly parse large sample CSV file', done => {
+    const onData = sinon.spy();
+    const input$ = of(...sampleCSVStrings);
+    const row$ = input$.pipe(parseCSV({
+      parserOptions: {
+        columns: true,
+        // quote: '"',
+        cast: true,
+        cast_date: false,
+        // delimiter: ',',
+        ltrim: true,
+        rtrim: true,
+        relax_column_count_more: true,
+        skip_empty_lines: true,
+      }
+    }));
+    row$.subscribe(onData, console.trace, () => {
+      expect(onData.callCount).to.equal(224);
+      expect(onData.getCall(223).args[0]).to.deep.equal({
+        _id: '61a7fd3ff3ab3994c246d596l1RyxfhBQYtf',
+        text: 'medical',
+        start: 76.75,
+        end: 77.15,
+        confidence: 1,
+        timestamp: 1638399407217,
+        sttEngine: 'tfEnsembler',
+        pipeline: 'stt',
+        i: 342,
+        speaker: '',
+        speakerConfidence: 0,
+      });
+      done();
+    });
+  }).timeout(5000);
+
+  // it('should handle error when input is a string but is not valid CSV', () => {
+  //   expect(false).to.be.true;
+  // });
+
+  // it('should handle error when input is not a String', () => {
+  //   expect(false).to.be.true;
+  // });
 
 
  //  it('should produce correct row objects when given simple CSV strings', marbles(m => {
